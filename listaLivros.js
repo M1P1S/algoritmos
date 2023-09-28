8c517db70a96f0928a027c11daf64ac42462489f
Matheus Pereira <matheusp135@gmail.com>
2023-09-28 09:58:22 -0300

    Algoritmos

---
+++ listaLivros.js
@@ -0,0 +1,24 @@
+//                    0   1   2   3   4   5
+const precosLivros = [
+  {
+    titulo: "JavaScript", 
+    preco: 25
+  }, 
+  {titulo: "PHP",
+    preco: 15
+  },
+  {titulo: "Java",
+   preco:30
+  },
+  {titulo: "Elixir",
+   preco: 50
+  },
+  {titulo: "Go",
+   preco: 45
+  },
+  {titulo: "Python",
+   preco: 20
+  }
+];
+
+module.exports = precosLivros
